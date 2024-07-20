0c62d01646be21ac37a01fe47d9ba089ca143cbe
Hera Chamorro <hera@hera.wtf>
2024-07-20 01:27:12 -0400

    Start working on constructors

--- src/dstr.rs
+++ src/dstr.rs
@@ -1,8 +1,11 @@
 use core::{
+    ffi::{c_char, CStr},
     num::NonZeroUsize,
     slice::{from_raw_parts, from_raw_parts_mut},
     str::{from_utf8, from_utf8_unchecked, from_utf8_unchecked_mut},
 };
+
+use crate::util;
 
 /// A nul-terminated UTF-8 string.
 #[repr(transparent)]
@@ -57,7 +60,7 @@ impl DStr {
     #[inline]
     #[must_use]
     pub const fn as_bytes_with_nul(&self) -> &[u8] {
-        unsafe { &*(self as *const DStr as *const [u8]) }
+        &self.bytes
     }
 
     /// Converts this [`DStr`] to a `&[u8]` excluding the nul
@@ -83,7 +86,7 @@ impl DStr {
     #[inline]
     #[must_use]
     pub unsafe fn as_bytes_with_nul_mut(&mut self) -> &mut [u8] {
-        unsafe { &mut *(self as *mut DStr as *mut [u8]) }
+        &mut self.bytes
     }
 
     /// Converts this [`DStr`] to a `&mut [u8]` excluding the nul
@@ -100,7 +103,10 @@ impl DStr {
     #[inline]
     #[must_use]
     pub unsafe fn as_bytes_mut(&mut self) -> &mut [u8] {
-        unsafe { from_raw_parts_mut(self.bytes.as_mut_ptr(), self.len()) }
+        let len = self.len();
+        let ptr = self.bytes.as_mut_ptr();
+
+        unsafe { from_raw_parts_mut(ptr, len) }
     }
 
     /// Converts this [`DStr`] to a `&str` containing the nul
@@ -166,5 +172,76 @@ impl DStr {
         );
 
         unsafe { from_utf8_unchecked_mut(self.as_bytes_mut()) }
+    }
+
+    /// Converts this [`DStr`] to a `&CStr`.
+    #[inline]
+    #[must_use]
+    pub const fn as_cstr(&self) -> &CStr {
+        unsafe { CStr::from_bytes_with_nul_unchecked(self.as_bytes_with_nul()) }
+    }
+
+    /// Converts this [`DStr`] to a raw pointer.
+    #[inline]
+    #[must_use]
+    pub const fn as_ptr(&self) -> *const u8 {
+        self.bytes.as_ptr()
+    }
+
+    /// Converts this [`DStr`] to a raw mutable pointer.
+    #[inline]
+    #[must_use]
+    pub fn as_ptr_mut(&mut self) -> *mut u8 {
+        self.bytes.as_mut_ptr()
+    }
+
+    /// Converts this [`DStr`] to a raw C string pointer.
+    #[inline]
+    #[must_use]
+    pub const fn as_c_ptr(&self) -> *const c_char {
+        self.as_ptr() as *const c_char
+    }
+
+    /// Converts this [`DStr`] to a raw mutable C string pointer.
+    #[inline]
+    #[must_use]
+    pub fn as_c_ptr_mut(&mut self) -> *mut c_char {
+        self.as_ptr() as *mut c_char
+    }
+}
+
+impl DStr {
+    #[inline]
+    #[must_use]
+    pub const unsafe fn from_bytes_with_nul_unchecked(bytes: &[u8]) -> &DStr {
+        unsafe { &*(bytes as *const [u8] as *const DStr) }
+    }
+
+    #[inline]
+    #[must_use]
+    pub unsafe fn from_bytes_with_nul_unchecked_mut(bytes: &mut [u8]) -> &mut DStr {
+        unsafe { &mut *(bytes as *mut [u8] as *mut DStr) }
+    }
+
+    #[inline]
+    #[must_use]
+    pub const unsafe fn from_raw<'a>(ptr: *const c_char) -> &'a DStr {
+        unsafe {
+            DStr::from_bytes_with_nul_unchecked(from_raw_parts(
+                ptr as *const u8,
+                util::strlen(ptr) + 1,
+            ))
+        }
+    }
+
+    #[inline]
+    #[must_use]
+    pub unsafe fn from_raw_mut<'a>(ptr: *mut c_char) -> &'a mut DStr {
+        unsafe {
+            DStr::from_bytes_with_nul_unchecked_mut(from_raw_parts_mut(
+                ptr as *mut u8,
+                util::strlen(ptr) + 1,
+            ))
+        }
     }
 }
